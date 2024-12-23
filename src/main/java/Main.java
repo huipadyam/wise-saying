40e376771327b37111da90a822df99b6b6edf115
kces0822 <kces0822@gmail.com>
2024-12-23 15:33:35 +0900

    등록 이이라고 치면 등록해줌

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,15 +10,25 @@ public class Main {
 class App{
     public void run(){
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("== 명언 앱 ==");
-        System.out.print("명언 : ");
-        scanner.nextLine(); // 입력값 가져옴. 입력값 없으면 기다린다.
-        System.out.print("작가 : ");
-        scanner.nextLine();
 
-        System.out.println("1번 명령이 등록되었습니다.");
+        while(true){
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
 
-        System.out.println("명령) 종료");
+            if(command.equals("등록")){ // 문자열 비교 시에는 .equals() 사용
+                System.out.print("명언 : ");
+                scanner.nextLine(); // 입력값 가져옴. 입력값 없으면 기다린다.
+
+                System.out.print("작가 : ");
+                scanner.nextLine();
+
+                System.out.println("1번 명령이 등록되었습니다.");
+            }
+
+
+            // System.out.println("명령) 종료");
+        }
+
     }
 }
