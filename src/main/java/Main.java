30e0353c5c599caf0600c69a4ef9a51d53c24270
kces0822 <kces0822@gmail.com>
2024-12-24 11:48:06 +0900

    객체 사용

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,9 +14,12 @@ class App{
         System.out.println("== wise-saying App ==");
 
         int lastNo = 0;
-        String content = "";
+        // String content = "";
         String[] contentList = new String[3];
         String[] authorList = new String[3];
+
+        WiseSaying wisesaying = new WiseSaying();
+
         while(true){
             System.out.print("command) ");
             String command = scanner.nextLine();
@@ -25,19 +28,24 @@ class App{
                 System.out.println("Quit wise-saying App");
                 break;
             } else if(command.equals("add")){ // 문자열 비교 시에는 .equals() 사용
-                System.out.print("comment : ");
-                contentList[lastNo] = scanner.nextLine(); // 입력값 가져옴. 입력값 없으면 기다린다.
+                System.out.print("content : ");
+                String content = scanner.nextLine(); // 입력값 가져옴. 입력값 없으면 기다린다.
 
                 System.out.print("author : ");
-                authorList[lastNo] = scanner.nextLine();
+                String author = scanner.nextLine();
 
-                System.out.println("%d(th) comment has been added".formatted(lastNo++));
+                wisesaying.id = lastNo;
+                wisesaying.author = author;
+                wisesaying.content = content;
+
+                System.out.println("%d(th) wisesaying has been added".formatted(lastNo++));
             } else if(command.equals("list")){
-                System.out.println("No. / author / comment");
+                System.out.println("No. / author / content");
                 System.out.println("---------------------------");
-                for(int i = 2; i >= 0; i++){
-                    System.out.println("%d / %s / %s".formatted(i, authorList[i], contentList[i]));
-                }
+                System.out.println("%d / %s / %s".formatted(wisesaying.id, wisesaying.author, wisesaying.content));
+//                for(int i = 2; i >= 0; i++){
+//                    System.out.println("%d / %s / %s".formatted(i, authorList[i], contentList[i]));
+//                }
 
             } else if(command.equals("delete")){
 
@@ -50,4 +58,10 @@ class App{
         }
 
     }
+}
+
+class WiseSaying{
+    int id;
+    String content;
+    String author;
 }
